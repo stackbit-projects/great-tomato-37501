b5e2613f2dbbe32323f01f367c75e79c4eef545f
Stackbit Code Editor <projects@stackbit.com>
2021-04-17 11:46:57 +0000

    post.js: changed

--- src/templates/post.js
+++ src/templates/post.js
@@ -10,7 +10,7 @@ import BlogPostFooter from '../components/BlogPostFooter';
 // any changes to content files are reflected in browser
 export const query = graphql`
   query($url: String) {
-    sitePage(path: {eq: $url}) {
+    sitePage(path: {eq: https://wpintegrate.net/wp-json/wp/v2/posts}) {
       id
     }
   }
