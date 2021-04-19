a3bbb81127aeb18fdd895be0d57c31f0cf2eba35
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 12:34:59 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -6,8 +6,7 @@ import _ from 'lodash';
 export default class Getposts extends React.Component {
     render() {
       return (
-            <footer className="post-meta">
-            </footer>
+            
         );
     }
 }
