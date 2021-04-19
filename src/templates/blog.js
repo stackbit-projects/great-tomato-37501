6460e22cf153868c82fe5c6ba6dc647a11d5524e
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 11:27:06 +0000

    blog.js: changed

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -41,6 +41,7 @@ export default class Blog extends React.Component {
                           <p>{_.get(post, 'frontmatter.excerpt', null)}</p>
                         </div>
                         <BlogPostFooter {...this.props} page={post} date_type={'short'} />
+                      <getPosts />
                       </div>
                     </div>
                   </article>
