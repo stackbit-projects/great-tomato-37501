c30fb20d55d80a42d0534ef34b78f06089496512
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 10:22:15 +0000

    post.js: changed

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,7 +5,7 @@ import {graphql} from 'gatsby';
 import {Layout} from '../components/index';
 import {htmlToReact, withPrefix} from '../utils';
 import BlogPostFooter from '../components/BlogPostFooter';
-import getPost from '../utils/getposts';
+import getPost from '../utils/getPosts';
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
