b43868c60e57b8f83852b186721d76cb1ee54a2b
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 12:20:02 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -6,8 +6,35 @@ import _ from 'lodash';
 export default class Getposts extends React.Component {
     render() {
       return (
-            <footer className="post-meta">
-            </footer>
+            fetch("https://wpintegrate.net/graphql", {
+    method: "POST",
+    headers: {
+    "content-type": "application/json",
+    },
+    body: JSON.stringify({
+        query: `{
+            products(first:5, after:""){
+            pageInfo {
+            hasNextPage
+            endCursor
+            }
+            nodes{
+            name
+            id
+            ... on SimpleProduct{
+            uri
+            price
+            }
+            }
+            }
+            }`,
+    }),
+})
+.then((res) => res.json())
+.then((res) => {
+    console.log(res);
+    return res;
+});
         );
     }
 }
