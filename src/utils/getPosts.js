7c8df04982b026a7b0015869aa7ddf9f538fcee5
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 12:19:14 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -16,33 +16,5 @@ export default class Getposts extends React.Component {
 }
 
 export default function Getposts(){
-    fetch("https://wpintegrate.net/graphql", {
-    method: "POST",
-    headers: {
-    "content-type": "application/json",
-    },
-    body: JSON.stringify({
-        query: `{
-            products(first:5, after:""){
-            pageInfo {
-            hasNextPage
-            endCursor
-            }
-            nodes{
-            name
-            id
-            ... on SimpleProduct{
-            uri
-            price
-            }
-            }
-            }
-            }`,
-    }),
-})
-.then((res) => res.json())
-.then((res) => {
-    console.log(res);
-    return res;
-});
+    
 }
