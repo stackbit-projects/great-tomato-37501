ea62d7267f13fb8fdd4b84fb0290b7c72533648b
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 14:53:25 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -1,28 +1,4 @@
-fetch("https://wpintegrate.net/graphql", {
-method: "POST",
-headers: {
-"content-type": "application/json",
-},
-body: JSON.stringify({
-query: `{
-products(first:5, after:""){
-pageInfo {
-hasNextPage
-endCursor
-}
-nodes{
-name
-id
-... on SimpleProduct{
-uri
-price
-}
-}
-}
-}`,
-}),
-})
-.then((res) => res.json())
-.then((res) => {
-console.log(res);
-});
+import React from "react"
+import { graphql } from "gatsby"
+
+
