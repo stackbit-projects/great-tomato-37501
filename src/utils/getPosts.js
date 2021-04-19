4bf43262a0cb57cbd11755146edeb2adb41062e9
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 14:53:27 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -1,4 +1,17 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+const Getposts = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
 
+export const query = graphql`
+  {
+    posts {
+      nodes {
+        id
+        title
+      }
+    }
+  }
+`
+
+export default Getposts
