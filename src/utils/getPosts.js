749524754ce9658292b6c69607bfcf0187b60965
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 12:38:29 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -6,7 +6,39 @@ import _ from 'lodash';
 export default class Getposts extends React.Component {
     render() {
       return (
-            
+           fetch("https://wpintegrate.net/graphql", {
+method: "POST",
+headers: {
+"content-type": "application/json",
+},
+body: JSON.stringify({
+query: `mutation LoginUser {
+login(input: {
+clientMutationId: "233",
+username: "${uname}",
+password: "${password}"
+}) {
+authToken
+clientMutationId
+user {
+name
+databaseId
+nicename
+}
+}
+}`,
+}),
+})
+.then((res) => res.json())
+.then((res) => {
+console.log(res);
+if (res.errors != undefined) {
+this.message = res.errors[0].message;
+} else if (res.data.login.authToken) {
+this.authToken = res.data.login.authToken;
+this.nicename = res.data.login.user.nicename;
+}
+}); 
         )
     }
 }
