77b6c5d2b9e6df8f74b37336ac73983bdf585350
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 11:14:50 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -2,5 +2,32 @@ import _ from 'lodash';
 
 
 export default function getPosts(){
-    
+    fetch("https://wpintegrate.net/graphql", {
+method: "POST",
+headers: {
+"content-type": "application/json",
+},
+body: JSON.stringify({
+query: `{
+products(first:5, after:""){
+pageInfo {
+hasNextPage
+endCursor
+}
+nodes{
+name
+id
+... on SimpleProduct{
+uri
+price
+}
+}
+}
+}`,
+}),
+})
+.then((res) => res.json())
+.then((res) => {
+console.log(res);
+});
 }
