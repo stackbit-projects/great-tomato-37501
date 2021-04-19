8e6268c66024f0eca6def2a8198da52e7e245ad0
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 11:15:26 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -9,21 +9,21 @@ headers: {
 },
     body: JSON.stringify({
         query: `{
-        products(first:5, after:""){
-        pageInfo {
-        hasNextPage
-        endCursor
-        }
-        nodes{
-        name
-        id
-        ... on SimpleProduct{
-        uri
-        price
-        }
-        }
-        }
-        }`,
+            products(first:5, after:""){
+            pageInfo {
+            hasNextPage
+            endCursor
+            }
+            nodes{
+            name
+            id
+            ... on SimpleProduct{
+            uri
+            price
+            }
+            }
+            }
+            }`,
     }),
 })
 .then((res) => res.json())
