5a26f25664ac9ac1980bfdd4af04464e1afd6eca
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 13:03:54 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -1,42 +1,17 @@
-import React from 'react';
-import _ from 'lodash';
+import React from "react"
+import { graphql } from "gatsby"
 
+const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
 
-
-export default class Getposts extends React.Component {
-    render() {
-      return (
-           fetch("https://wpintegrate.net/graphql", {
-method: "POST",
-headers: {
-"content-type": "application/json",
-},
-body: JSON.stringify({
-query: `mutation LoginUser {
-login(input: {
-clientMutationId: "233",
-username: "chunt@dwsnow.com",
-password: "C8JUBYqXtT5oQQ(&!fYbx3"
-}) {
-authToken
-clientMutationId
-user {
-name
-databaseId
-nicename
-}
-}
-}`,
-}),
-})
-.then((res) => res.json())
-.then((res) => {
-console.log(res);
-if (res.errors != undefined) {
-    this.message = res.errors[0].message;
-} 
-}) 
-        )
+export const query = graphql`
+  {
+    posts {
+      nodes {
+        id
+        title
+      }
     }
-}
+  }
+`
 
+export default ComponentName
