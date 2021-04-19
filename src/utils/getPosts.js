dfb6b18f0110b30b282de7b19e5651f3d8b04f65
Stackbit Code Editor <projects@stackbit.com>
2021-04-19 12:19:45 +0000

    getPosts.js: changed

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -5,9 +5,7 @@ import _ from 'lodash';
 
 export default class Getposts extends React.Component {
     render() {
-       
-        let date_type = _.get(this.props, 'date_type', null);
-        return (
+      return (
             <footer className="post-meta">
                 <time className="published" dateTime={moment(_.get(post, 'frontmatter.date', null)).strftime('%Y-%m-%d %H:%M')}>{(date_type === 'short') ? (moment(_.get(post, 'frontmatter.date', null)).strftime('%B %d, %Y')) : moment(_.get(post, 'frontmatter.date', null)).strftime('%A, %B %e, %Y')}</time>{_.get(post, 'frontmatter.author', null) && ((() => {    let author = getData(this.props.pageContext.site.data, _.get(post, 'frontmatter.author', null));    return (', by ' + author.first_name  + author.last_name);})())}
             </footer>
